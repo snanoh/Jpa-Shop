540b0293191a8f5e3474b147f5bd7fb9886252fc
User <njs1324@gmail.com>
2021-04-17 22:12:16 +0900

    Entity 생성

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+
+    //JPA 때만 사용하도록 protected로 막음 private는 JPA에서 Embeded에서 제공하지 않음
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode){
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}

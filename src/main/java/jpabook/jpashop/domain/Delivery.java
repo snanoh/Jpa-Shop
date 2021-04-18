30859b991fc043a9e9286100d656a11d4ec2627f
User <njs1324@gmail.com>
2021-04-18 12:03:16 +0900

    Fetch Type 설정(LAZY)

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -15,7 +15,7 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = fetchType.LAZY)
     private Order order;
 
     @Embedded
